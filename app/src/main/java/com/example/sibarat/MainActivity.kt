6039ebe0ca7f38a178567d3ce2526fb1d572596a
raziqaizza <raziqaizza@gmail.com>
2024-05-31 22:49:42 +0700

    Update Design on 31 Mei 2024 22:49 (activity_main and activity_camera)

--- app/src/main/java/com/example/sibarat/MainActivity.kt
+++ app/src/main/java/com/example/sibarat/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.sibarat
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sibarat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        setupButton()
+    }
+
+    private fun setupButton() {
+        binding.btnScanFoto.setOnClickListener {
+            val scanIntent = Intent(this@MainActivity, CameraActivity::class.java)
+            startActivity(scanIntent)
+        }
+
+        binding.btnKamus.setOnClickListener {
+            val kamusIntent = Intent(this@MainActivity, DictionaryActivity::class.java)
+            startActivity(kamusIntent)
+        }
     }
 }
